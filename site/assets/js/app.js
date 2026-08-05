f667ac808715a133e539e967e66c1eb7caf6b309
Itzjac Lechtman <itzjaclechtman@macbookpro.mynetworksettings.com>
2026-08-04 20:45:15 -0400

    Bust cached telemetry page assets

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -1,3 +1,3 @@
-import { initRouter } from './router.js';
+import { initRouter } from './router.js?v=20260805d';
 
 initRouter();
--- site/assets/js/router.js
+++ site/assets/js/router.js
@@ -1,9 +1,11 @@
-import { initI2CPage, destroyI2CPage } from './i2c.js';
+import { initI2CPage, destroyI2CPage } from './i2c.js?v=20260805d';
+
+const assetVersion = '20260805d';
 
 const routes = {
-  home: './modules/home.html',
-  communication: './modules/communication.html',
-  partners: './modules/partners.html'
+  home: `./modules/home.html?v=${assetVersion}`,
+  communication: `./modules/communication.html?v=${assetVersion}`,
+  partners: `./modules/partners.html?v=${assetVersion}`
 };
 
 const pageInitializers = {
